2cf68e051b32b6948fa9607fca7632126f55e5e1
zohaibamjad1003 <zohaibamjad@hof-global.com>
2026-07-24 23:22:27 +0500

    feat: connect MongoDB Atlas to Backend

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
+
+
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected!"))
+  .catch((err) => console.log("MongoDB error:", err));
 app.use(cors());
 app.use(express.json());
 
@@ -12,3 +20,5 @@ app.get('/api/test', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
